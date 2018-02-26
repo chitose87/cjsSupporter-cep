ec655ba9395229dace95cc6bd9bdbf16b63ff536
pon87 <chitose87@gmail.com>
2018-02-26 18:47:52 +0900

    α0

---
+++ sample/sample_01.js
@@ -0,0 +1,175 @@
+// Converted with cjsSupporter
+
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [{"name":"rect-triangle","frames":[[0,0,121,121],[0,121,181,157]]}];
+
+
+// symbols:
+
+
+
+(lib.circle = function() {
+	this.initialize(img.circle);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,191,191);
+
+
+(lib.rect = function() {
+this.spriteSheet = ss["rect-triangle"];
+this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.triangle = function() {
+this.spriteSheet = ss["rect-triangle"];
+this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.triangle_mc = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー_1
+	this.instance = new lib.triangle();
+	this.instance.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.triangle_mc, new cjs.Rectangle(0,0,181,157), null);
+
+
+(lib.rect_mc = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー_1
+	this.instance = new lib.rect();
+	this.instance.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.rect_mc, new cjs.Rectangle(0,0,121,121), null);
+
+
+(lib.cycle_mc = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー_1
+	this.instance = new lib.circle();
+	this.instance.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.cycle_mc, new cjs.Rectangle(0,0,191,191), null);
+
+
+// stage content:
+(lib.sample_01 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー_1
+	this.instance = new lib.cycle_mc();
+	this.instance.parent = this;
+	this.instance.setTransform(178.5,293.5,1,1,0,0,0,95.5,95.5);
+
+	this.instance_1 = new lib.triangle_mc();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(372.5,150.5,1,1,0,0,0,90.5,78.5);
+
+	this.instance_2 = new lib.rect_mc();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(134.5,92.5,1,1,0,0,0,60.5,60.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(349,232,389,357);
+// library properties:
+lib.properties = {
+	id: '56148CE10DB86049A5424705223DAA33',
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+	    {"src":"images/circle.png","id":"circle"}
+	    ,{"src":"rect-triangle.png","id":"rect-triangle"}],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['56148CE10DB86049A5424705223DAA33'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -6,11 +6,13 @@
                 th Watch
                 td
                     input#hoge(type="checkbox",v-model="watch")
+                td
 
             tr.target
                 th Target
                 td
                     input(type="text",v-model="flaData.targetPath",@keyup="targetPathUpdate")
+                td(v-bind:data-status="targetPathStatus")
 
             tr.list
                 td(colspan=2)
@@ -25,12 +27,7 @@
                         tr(v-for="item in flaData.ssList")
                             td
                                 input(type="text",placeholder="http://",v-model="item.path",@keyup="ssListUpdate")
-                            td
-                                button() <
-                            td
-                                button() >
-                            td
-                                button() ×
+                            td(v-bind:data-status="item.status")
 
             tr.deploy(data-js="deploy")
                 th Deploy
@@ -38,6 +35,29 @@
                     button.topcoat-button.hostFontSize Deploy
 </template>
 <style lang="scss" scoped>
+    //success
+    [data-status="200"]:before {
+        content: '200';
+        color: green;
+    }
+
+    //Found
+    [data-status="302"]:before {
+        content: '302';
+        color: green;
+    }
+
+    //Bad Request
+    [data-status="400"]:before {
+        content: '400';
+        color: red;
+    }
+
+    //Not Found
+    [data-status="404"]:before {
+        content: '404';
+        color: red;
+    }
 
 </style>
 <script lang="ts">
@@ -55,19 +75,22 @@
         target: string = "hoge";
         flaPath: string = "";
         flaName: string = "";
+        targetPathStatus: number = 404;
 
         private watcher: any;
+        private timeoutId: number;
 
         private updateOption() {
             fl.flaDataUpdate(this.flaData);
         }
 
         targetPathUpdate() {
+
             if (this.watcher) this.watcher.close();
 
-//            let fs = eval("require('fs')");
-            let path = this.flaPath + this.flaData.targetPath;
+            let path = this.flaPath + "/" + this.flaData.targetPath;
             try {
+                this.targetPathStatus = 200;
                 fs.accessSync(path);
                 let id = 0;
                 this.watcher = fs.watch(path, () => {
@@ -75,7 +98,8 @@
                     id = setTimeout(() => this.onTargetUpdate(), 500);
                 });
             } catch (e) {
-                console.log("not file", path);
+                this.targetPathStatus = 404;
+                fl.run(`fl.trace('Not Found:${path}')`);
             }
 
             this.updateOption();
@@ -83,15 +107,110 @@
 
         ssListUpdate() {
             let ssList: any[] = this.flaData.ssList;
+
             if (ssList.length == 0 || ssList[ssList.length - 1].path != "") {
-                ssList.push({path: ""});
+                ssList.push({path: "", status: 0});
             }
+
+            for (let item: any of ssList) {
+                if (item.path == "") {
+                    item.status = 0;
+                } else if (!item.path.match(/\.(json)$/i)) {
+                    item.status = 400;
+                } else {
+                    let path = this.flaPath + "/" + item.path;
+                    try {
+                        item.status = 200;
+                        fs.accessSync(path);
+                    } catch (e) {
+                        item.status = 404;
+                        fl.run(`fl.trace('Not Found:${path}')`);
+                    }
+                }
+            }
+
             this.updateOption();
         }
 
         onTargetUpdate() {
-            let v = fs.readFileSync(this.flaPath + this.flaData.targetPath, 'utf8');
-            console.log(v);
+            console.log("onTargetUpdate");
+            // check founded
+            if (this.targetPathStatus != 200) {
+                return fl.run(`fl.trace('Targetの設定に問題があります。')`);
+            }
+            let ssList: any[] = this.flaData.ssList;
+            for (let item: any of ssList) {
+                if (item.status > 0 && item.status != 200) {
+                    return fl.run(`fl.trace('スプライトシートの設定に問題があります。')`);
+                }
+            }
+
+            let targetPath = this.flaPath + "/" + this.flaData.targetPath;
+            let targetJs: string = fs.readFileSync(targetPath, 'utf8');
+            if (targetJs.indexOf("// Converted with cjsSupporter") >= 0) {
+                return fl.run(`fl.trace('cjsSupporter Converted')`);
+            }
+
+            // all ready
+            let _ssMetadata = [];
+            window.targetJs = targetJs;
+            let _manifest = eval(targetJs.match(/manifest: ([^\]]+)/)[1] + "]");
+
+            try {
+                for (let item: any of ssList) {
+                    // a sprite sheet
+                    if (item.status == 0) continue;
+                    let ssJson: any = JSON.parse(
+                        fs.readFileSync(
+                            this.flaPath + "/" + item.path
+                            , 'utf8')
+                            .match(/{([^?]+)/)[0]
+                    );
+                    let result = {
+                        name: ssJson.meta.image.match(/(.*)(?:\.([^.]+$))/)[1],
+                        frames: []
+                    };
+                    let index = 0;
+                    for (let key in ssJson.frames) {
+                        // a frame
+                        let _name = key.match(/(.*)(?:\.([^.]+$))/)[1];
+                        let frame = ssJson.frames[key];
+                        result.frames.push([frame.frame.x, frame.frame.y, frame.frame.w, frame.frame.h]);
+
+                        let s = targetJs.indexOf(`.${_name} = function`);
+                        s = targetJs.indexOf(`{`, s) + 1;
+                        let e = targetJs.indexOf(`new cjs.Rectangle(`, s);
+                        e = targetJs.indexOf(`;`, e) + 1;
+                        targetJs = targetJs.replace(targetJs.substring(s, e),
+                            `
+this.spriteSheet = ss["${result.name}"];
+this.gotoAndStop(${index});
+}).prototype = p = new cjs.Sprite();
+`);
+                        // splice manifest
+                        _manifest.some((v: any) => {
+                            if (v.id == _name) {
+                                _manifest.splice(_manifest.indexOf(v), 1);
+                                return true;
+                            }
+                        });
+                        index++;
+                    }
+                    _ssMetadata.push(result);
+                    _manifest.push({
+                        src: ssJson.meta.image
+                        , id: result.name
+                    })
+                }
+                targetJs = "// Converted with cjsSupporter\n\r"
+                    + targetJs.replace(/ssMetadata([^;]+)/, "ssMetadata = " + JSON.stringify(_ssMetadata))
+                        .replace(/manifest: ([^\]]+)]/, "manifest: " + JSON.stringify(_manifest));
+
+                // write
+                fs.writeFileSync(targetPath, targetJs);
+            } catch (e) {
+                console.log(e);
+            }
         }
 
         set watch(v: boolean) {
